67262c5d54dca5984a40e8034e550dd768a96a9f
Ali_Shair <alishair7071@gmail.com>
2025-09-07 17:21:00 +0500

    changed the server url

--- Frontend/src/server.js
+++ Frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server=  "http://localhost:8000/api/v2";
+export const server=  "https://multivendor-inky.vercel.app/api/v2";
 
-export const backend_url= "http://localhost:8000/";
+export const backend_url= "https://multivendor-inky.vercel.app/";
