3dddaeda27399f4ea256268d905b6657f5bcc3d7
Ali_Shair <alishair7071@gmail.com>
2025-09-10 17:35:04 +0500

    make the strings in cloudinary config

--- Backend/api/app.js
+++ Backend/api/app.js
@@ -39,9 +39,9 @@ app.use("/test", (req, res) => {
 connectDatabase();
 
 cloudinary.config({
-  cloud_name: dqf4fxp4x,
-  api_key: 744127157597433,
-  api_secret: NuleBAZD0CDcgJseAwphgug2W0w,
+  cloud_name: "dqf4fxp4x",
+  api_key: "744127157597433",
+  api_secret: "NuleBAZD0CDcgJseAwphgug2W0w",
 });
 
 // Routes
