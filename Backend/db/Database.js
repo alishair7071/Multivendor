773db56b70f1bb11862ef80fb5cef9c7b5cb3282
Ali_Shair <alishair7071@gmail.com>
2025-09-11 16:02:54 +0500

    changed the db url

--- Backend/db/Database.js
+++ Backend/db/Database.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose
-    .connect(process.env.DB_URL, {
-      useNewUrlParser: true
-    })
+    .connect(
+      "mongodb+srv://alishair7071:O6Mqg3Vutby5DBib@cluster0.uxswtqf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      {
+        useNewUrlParser: true,
+      }
+    )
     .then((data) => {
-      console.log(`mongodb is connnected with server`);
+      console.log(`mongodb is connnected with server ++`);
     })
     .catch((err) => {
       console.log("does not connected with mongodb due to: " + err.message);
