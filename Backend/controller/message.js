900625d18ec6fb1bd9bec919677940e37e59b3b6
Ali_Shair <alishair7071@gmail.com>
2025-09-12 12:03:30 +0500

    change the create message logic

--- Backend/controller/message.js
+++ Backend/controller/message.js
@@ -14,38 +14,55 @@ router.post(
     try {
       const messageData = {};
 
-      // Upload image to Cloudinary
-      const uploadStream = cloudinary.v2.uploader.upload_stream(
-        { folder: "messages_image" },
-        async (error, result) => {
-          if (error) return next(new ErrorHandler(error.message, 500));
+      //if message comes image
+      if (req.file) {
+        // Upload image to Cloudinary
+        const uploadStream = cloudinary.v2.uploader.upload_stream(
+          { folder: "messages_image" },
+          async (error, result) => {
+            if (error) return next(new ErrorHandler(error.message, 500));
 
-          // Store uploaded image info
-          const uploadedImage = {
-            public_id: result.public_id,
-            url: result.secure_url,
-          };
+            // Store uploaded image info
+            const uploadedImage = {
+              public_id: result.public_id,
+              url: result.secure_url,
+            };
 
-          // Build message data
-          messageData.images = uploadedImage;
-          messageData.conversationId = req.body.conversationId;
-          messageData.sender = req.body.sender;
-          messageData.text = req.body.text;
+            // Build message data
+            messageData.images = uploadedImage;
+            messageData.conversationId = req.body.conversationId;
+            messageData.sender = req.body.sender;
+            messageData.text = req.body.text;
 
-          // Save message in DB
-          const message = new Messages(messageData);
-          await message.save();
+            // Save message in DB
+            const message = new Messages(messageData);
+            await message.save();
 
-          // Send response
-          res.status(201).json({
-            success: true,
-            message,
-          });
-        }
-      );
+            // Send response
+            res.status(201).json({
+              success: true,
+              message,
+            });
+          }
+        );
+        // Pipe the buffer to cloudinary (starts upload)
+        uploadStream.end(req.file.buffer);
+      } else {
+        //if message comes without image
+        messageData.conversationId = req.body.conversationId;
+        messageData.sender = req.body.sender;
+        messageData.text = req.body.text;
 
-      // Pipe the buffer to cloudinary (starts upload)
-      uploadStream.end(req.file.buffer);
+        // Save message in DB
+        const message = new Messages(messageData);
+        await message.save();
+
+        // Send response
+        res.status(201).json({
+          success: true,
+          message,
+        });
+      }
     } catch (error) {
       return next(new ErrorHandler(error.message, 500));
     }
