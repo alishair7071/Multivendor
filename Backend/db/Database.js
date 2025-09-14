7af951fb7047323b6ba21700e5f9e7773e149803
Ali_Shair <alishair7071@gmail.com>
2025-09-14 23:56:12 +0500

    again

--- Backend/db/Database.js
+++ Backend/db/Database.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose
-    .connect("mongodb://alishair7071:FlutterNodeReact@ac-jh6zjts-shard-00-00.oqi8n7w.mongodb.net:27017,ac-jh6zjts-shard-00-01.oqi8n7w.mongodb.net:27017,ac-jh6zjts-shard-00-02.oqi8n7w.mongodb.net:27017/multivendor_db?ssl=true&replicaSet=atlas-sackt4-shard-0&authSource=admin&retryWrites=true&w=majority&appName=SocialMedia-cluster",
+    .connect(
+      "mongodb://alishair7071:FlutterNodeReact@ac-jh6zjts-shard-00-00.oqi8n7w.mongodb.net:27017,ac-jh6zjts-shard-00-01.oqi8n7w.mongodb.net:27017,ac-jh6zjts-shard-00-02.oqi8n7w.mongodb.net:27017/?ssl=true&replicaSet=atlas-sackt4-shard-0&authSource=admin&retryWrites=true&w=majority&appName=SocialMedia-cluster",
       {
         useNewUrlParser: true,
       }
