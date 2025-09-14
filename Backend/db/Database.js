794e008234d05b24ac67d4227d0d0ccc7d8d36d8
Ali_Shair <alishair7071@gmail.com>
2025-09-15 01:20:42 +0500

    change db settings

--- Backend/db/Database.js
+++ Backend/db/Database.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 
-const connectDatabase = () => {
-  mongoose
-    .connect(
-      "mongodb://alishair7071:FlutterNodeReact@ac-jh6zjts-shard-00-00.oqi8n7w.mongodb.net:27017,ac-jh6zjts-shard-00-01.oqi8n7w.mongodb.net:27017,ac-jh6zjts-shard-00-02.oqi8n7w.mongodb.net:27017/?ssl=true&replicaSet=atlas-sackt4-shard-0&authSource=admin&retryWrites=true&w=majority&appName=SocialMedia-cluster",
-      {
-        useNewUrlParser: true,
-      }
-    )
-    .then((data) => {
-      console.log(`mongodb is connnected with server ++`);
-    })
-    .catch((err) => {
-      console.log("does not connected with mongodb due to: " + err.message);
-    });
+const connectDatabase = async () => {
+  try {
+    await mongoose.connect("mongodb://alishair7071:FlutterNodeReact@ac-jh6zjts-shard-00-00.oqi8n7w.mongodb.net:27017,ac-jh6zjts-shard-00-01.oqi8n7w.mongodb.net:27017,ac-jh6zjts-shard-00-02.oqi8n7w.mongodb.net:27017/?ssl=true&replicaSet=atlas-sackt4-shard-0&authSource=admin&retryWrites=true&w=majority&appName=SocialMedia-cluster");
+    console.log("connected with mongodb successfully");
+      // added this by default data when server was run first time 
+      // now this data is stored on mongodb for testing and other purpose
+      // and now i am commenting it to avoid from adding frequently when server will run;
+      /*userModel.insertMany(users);
+      postModel.insertMany(posts);*/
+  } catch (e) {
+    console.log("cannot connect with mongodb: "+e);
+  }
 };
 
 module.exports = connectDatabase;
+
+ 
