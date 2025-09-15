2ed874d6c99490959c39831f436770b9593514d2
Ali_Shair <alishair7071@gmail.com>
2025-09-15 16:38:13 +0500

    buffer command false

--- Backend/db/Database.js
+++ Backend/db/Database.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
   try {
-    await mongoose.connect("mongodb+srv://alishair3420_db_user:FtrVf6RqBoxIVh6z@multivendor-cluster.lbkslhr.mongodb.net/?retryWrites=true&w=majority&appName=multivendor-cluster");
+    await mongoose.connect("mongodb+srv://alishair3420_db_user:FtrVf6RqBoxIVh6z@multivendor-cluster.lbkslhr.mongodb.net/?retryWrites=true&w=majority&appName=multivendor-cluster",{bufferCommands: false});
+    
+    
+  
     console.log("connected with mongodb successfully+++");
   } catch (e) {
     console.log("cannot connect with mongodb: "+e);
