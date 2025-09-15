f5b019a0c00ca66ccb8c1629bd47deff9989b137
Ali_Shair <alishair7071@gmail.com>
2025-09-15 15:55:53 +0500

    ...

--- Frontend/src/server.js
+++ Frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server=  "https://multivendor-backend-opal.vercel.app/api/v2";
+export const server=  "https://multivendor-two.vercel.app/api/v2";
 
 export const backend_url= "https://multivendor-inky.vercel.app/";
