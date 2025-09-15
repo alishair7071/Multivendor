0bbbb89831c7fa2f83dab96d750a354f51f8bc1e
Ali_Shair <alishair7071@gmail.com>
2025-09-15 13:09:07 +0500

    again frontend

--- Frontend/src/server.js
+++ Frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server=  "https://multivendor-backend-nine.vercel.app//api/v2";
+export const server=  "https://multivendor-inky.vercel.app/api/v2";
 
 export const backend_url= "https://multivendor-inky.vercel.app/";
