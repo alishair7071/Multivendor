fdc4e6c39d23f49024608c5a3d7fcd147d7c474d
Ali_Shair <alishair7071@gmail.com>
2025-09-16 13:07:52 +0500

    send mail secure true

--- Backend/utils/sendMail.js
+++ Backend/utils/sendMail.js
@@ -4,7 +4,8 @@ const sendMail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMPT_HOST,
     port: process.env.SMPT_PORT,
-    service: process.env.SMPT_SERVICE,
+    secure: true,
+    //service: process.env.SMPT_SERVICE,
     auth: {
       user: process.env.SMPT_MAIL,
       pass: process.env.SMPT_PASSWORD,
