74edc593b8a0d827094374f8c10740c15889491e
Ali_Shair <alishair7071@gmail.com>
2025-09-18 18:19:52 +0500

    updated db url

--- Backend/db/Database.js
+++ Backend/db/Database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
   try {
-    await mongoose.connect("mongodb+srv://alishair3420_db_user:FtrVf6RqBoxIVh6z@multivendor-cluster.lbkslhr.mongodb.net/?retryWrites=true&w=majority&appName=multivendor-cluster");
+    await mongoose.connect(process.env.DB_URL);
     
     console.log("connected with mongodb successfully+++");
   } catch (e) {
